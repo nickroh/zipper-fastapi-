6c235c013e1b3c0ea9c1f7de04a190a076a2d92d
nickroh <48642955+nickroh@users.noreply.github.com>
2023-11-19 07:43:20 +0000

    add database

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import databases
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
